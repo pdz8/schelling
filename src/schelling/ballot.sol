81c18c226e2a4384b90842287cf80c6662b8157e
Peter Zieske <pdz8@cornell.edu>
2015-03-01 23:37:03 -0500

    schelling pre-compile

---
+++ src/schelling/ballot.sol
@@ -0,0 +1,129 @@
+// Schelling Coin implementation
+
+// Schelling ballot that does not break values into binary representation
+contract OptionBallot {
+
+
+  // Authority which chooses voters
+  address authorities;
+
+  // Choice range (0 is not at option)
+  uint256 maxOption;
+
+  // Voting down payment
+  uint256 downPayment;
+  uint256 reward;
+
+  // Ballot schedule
+  uint256 startTime;
+  uint256 revealTime;
+  uint256 redeemTime;
+
+  // TODO
+  // When array support comes out allow the question string to be stored
+
+  // List of address that have submitted hashes
+  struct Participant {
+    hash256 h;
+    uint256 choice;
+    bool redeemed;
+  }
+  mapping (address => hash256) voterMap;
+
+  // Vote tallying
+  mapping (uint256 => uint256) tally;
+  uint256 numTallied;
+  uint256 decision;
+  uint256 numRedeemed;
+
+
+  // Constructor
+  function OptionBallot(
+      address _authorities, uint256 _maxOption, uint256 _downPayment,
+      uint256 _startTime, uint256 _votingPeriod, uint256 _revealPeriod,
+      ) {
+    authorities = _authorities;
+    maxOption = _maxOption;
+    downPayment = _downPayment;
+    startTime = _startTime;
+    revealTime = startTime + _votingPeriod;
+    revealPeriod = revealTime + _revealPeriod;
+  }
+
+
+  // Submit hash of vote
+  function submit_hash(hash256 h) {
+    if (block.timestamp < startTime || block.timestamp >= revealTime) return;
+    if (voterMap[tx.origin].h != 0x0) return;
+    if (!I_VoterPool(authorities).is_voter(tx.origin)) return;
+    if (msg.value < downPayment) return;
+    voterMap[tx.origin].h = h;
+  }
+
+
+  // Reveal hash value and tally the vote
+  function reveal_vote(uint256 voteVal, uint256 key) {
+
+    // Validate input
+    if (block.timestamp < revealTime || block.timestamp >= redeemTime) return;
+    if (voteVal == 0 || voteVal > maxOption) return;
+    // if (voterMap[tx.origin].h == 0x0) return;
+
+    // Check hash and vote if good
+    hash256 h = sha256(tx.origin, address(this), voteVal, key);
+    if (voterMap[tx.origin].h != h) return;
+    voterMap[tx.origin].choice = voteVal;
+    tally[voteVal]++;
+    numTallied++;
+
+    // Recalculate reward
+    reward = address(this).balance / numTallied;
+  }
+
+
+  // Get rewarded for vote
+  function redeem() {
+
+    // Validate input
+    if (block.timestamp < redeemTime) return;
+
+    // Choose winner if necessary
+    if (decision == 0) {
+      uint256 i = 2;
+      decision = 1;
+      while (i <= maxOption) {
+        if (tally[i] > decision[i]) {
+          decision = i;
+        }
+        i++;
+      }
+      if (decision == 0) return;
+    }
+
+    // Reward voter
+    if (voterMap[tx.origin].choice == decision) {
+      voterMap[tx.origin].h = 0;
+      voterMap[tx.origin].choice == 0;
+      tx.origin.send(reward);
+      numRedeemed++;
+      if (numRedeemed == numTallied) {
+        suicide(authorities);
+      }
+    }
+  }
+
+
+  // Debug only
+  function kill_me() {
+    suicide(tx.origin);
+  }
+
+}
+
+
+// Voter pool interface
+contract I_VoterPool {
+  function is_voter(address entry) returns(bool ret) {
+    return true;
+  }
+}
---
+++ src/schelling/blacklist.sol
@@ -0,0 +1,31 @@
+// Editable list of legal voters
+// Meant to serve as an interface that schelling decisions can easily follow
+// Implementations can either be whitelists or blacklists
+contract VoterPool {
+
+  address owner;
+  mapping (address => bool) blacklisted;
+
+  // Initialize pool with trusted owner
+  function VoterPool {
+    owner = msg.sender;
+  }
+
+  // Is this a voter
+  function is_voter(address entry) returns(bool ret) {
+    return !blacklisted[entry];
+  }
+
+  // Blacklist the voter
+  function blacklist(address entry) {
+    if (msg.sender != owner) return;
+    blacklisted[entry] = true;
+  }
+
+  // Pool destruction
+  function kill_me() {
+    if (msg.sender != owner) return;
+    suicide(owner);
+  }
+}
+
