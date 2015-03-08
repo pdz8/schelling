e3517b2795a7dd847f2a1e15b5a14e240b906c70
Peter Zieske <pdz8@cornell.edu>
2015-03-08 16:20:30 -0400

    account select done

--- src/clientsite/schelling.js
+++ src/clientsite/schelling.js
@@ -1,20 +1,42 @@
 var app = angular.module('SchellApp', []);
 
-app.controller('UserAccount', ['$scope','ethService',function($scope,es) {
-  $scope.bal = 0;
-  $scope.accs = es.accounts;
-  $scope.es = es;
-  $scope.changeAcc = function() { 
-    $scope.bal = es.getBalance($scope.acc)
-  };
+
+// Controls the user account panel
+app.controller('UserAccount', 
+        ['$scope','ethService','shareData',
+        function($scope,es,sd) {
+    $scope.accs = es.accounts;
+    $scope.setAccount = function(a) {
+        if ($scope.acc != a) { $scope.acc = a; }
+        sd.account = a;
+        $scope.bal = new BigNumber(es.getBalance(a)).toString(10);
+    };
+    $scope.setAccount(es.coinbase);
 }]);
 
+
+// Provides (wrapped) Ethereum functions
 app.factory('ethService', [function() {
-  var w = require('web3');
-  w.setProvider(new w.providers.HttpSyncProvider('http://localhost:8080')); // 8080 for cpp/AZ, 8545 for go/mist
-  return {
-    accounts: w.eth.accounts,
-    getBalance: w.eth.balanceAt
-  };
+    // var bn = require('BigNumber');
+    var w = require('web3');
+    w.setProvider(new w.providers.HttpSyncProvider('http://localhost:8080')); // 8080 for cpp/AZ, 8545 for go/mist
+    return {
+        // bn: bn,
+        accounts: w.eth.accounts,
+        getBalance: w.eth.balanceAt,
+        coinbase: w.eth.coinbase
+    };
 }]);
+
+
+// Allows data to be shared between controllers
+app.factory('shareData', [function() {
+    return {
+        account: "",
+        contract: ""
+    };
+}]);
+
+
+
 
