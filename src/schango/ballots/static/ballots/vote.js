bc3d7887d1bdcd8c50b1d1afe522b40926a137c7
Peter Zieske <pdz8@cornell.edu>
2015-03-18 03:38:42 -0400

    show details

---
+++ src/schango/ballots/static/ballots/vote.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    // Enable all the Ethereum details to be read
+    var switcher = $("#show_more");
+    switcher.click(function(){
+        if (switcher.text() == "Show more") {
+            switcher.text("Show less")
+        } else {
+            switcher.text("Show less")
+        }
+        $("#tech_panel").toggle();
+    });
+});
